26841cb5634a2ecde0226d9487057c61e79e53cc
zdulak <dpzdulski@gmail.com>
2022-04-06 09:54:45 +0200

    Change app name. Add inferSchema

--- src/main/scala/Wed.scala
+++ src/main/scala/Wed.scala
@@ -1,16 +1,16 @@
 import org.apache.spark.sql.functions._
 
 
-object Wed  extends App {
+object Wed extends App {
   import org.apache.spark.sql.SparkSession
 
   val spark = SparkSession
     .builder()
-    .appName("Spark SQL basic example")
+    .appName("Wed")
     .master("local[*]")
     .getOrCreate()
 
-  val dataFrame = spark.read.option("header", true).csv("data.csv")
+  val dataFrame = spark.read.option("header", true).option("inferSchema", true).csv("data.csv")
   val newFrame = dataFrame.columns.foldLeft(dataFrame)((frame, name) => frame.withColumn(name, upper(col(name))))
   newFrame.show()
 

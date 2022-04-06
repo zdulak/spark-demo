f15591d16571e267fccfedc0de5373e5bb4157e6
zdulak <dpzdulski@gmail.com>
2022-04-06 09:47:59 +0200

    Add .gitignore

--- src/main/scala/Wed.scala
+++ src/main/scala/Wed.scala
@@ -10,7 +10,7 @@ object Wed  extends App {
     .master("local[*]")
     .getOrCreate()
 
-  val dataFrame = spark.read.csv("data.csv")
+  val dataFrame = spark.read.option("header", true).csv("data.csv")
   val newFrame = dataFrame.columns.foldLeft(dataFrame)((frame, name) => frame.withColumn(name, upper(col(name))))
   newFrame.show()
 
